59d42e6cff6ac319259bd3e39becc0c521500c61
Hans Kong <hans.kong@zoom.us>
2024-02-28 14:00:53 +0800

    my3

--- models/db.ts
+++ models/db.ts
@@ -4,9 +4,9 @@ let globalPool: Pool;
 
 export function getDb() {
   if (!globalPool) {
-    // const connectionString = process.env.POSTGRES_URL;
-     const connectionString =
-       "postgres://postgres:123456@127.0.0.1:5432/postgres";
+    const connectionString = process.env.POSTGRES_URL;
+    //  const connectionString =
+      //  "postgres://postgres:123456@127.0.0.1:5432/postgres";
     console.log("connectionString", connectionString);
 
     globalPool = new Pool({
